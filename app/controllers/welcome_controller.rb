950630ce45dc65e95f91730c97bcc61114bbbc5e
Csaba Thamo <csabathamo86@gmail.com>
2016-09-15 21:02:48 +0200

    Main page text and showing 5 posts per page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 class WelcomeController < ApplicationController
 
 	def index
-		@posts = Post.all.order("created_at desc").limit(3)
-		@projects = Project.all.order("created_at desc").limit(3)
+		@posts = Post.all.order("created_at desc").limit(5)
+		@projects = Project.all.order("created_at desc").limit(5)
 	end
 
 end
